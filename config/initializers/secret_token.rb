1710154da45989d11d3e9bf3dcd2d29abed91499
waleed <waleed@waleed-HP.(none)>
2014-02-06 02:44:17 +0000

    demo of remote storage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '35eb710172b86bc1aa09864bb9daf3f4c511fc8bec5fd10da4e2dafd87e8096cc53635e2efc37cac3ab56506bba4e9bb91b22e165440f0a533f7dfb81d660c33'
